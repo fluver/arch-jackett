b1928c5600091b61c34d4c60eb43b3705d9fa585
flix <flix@localhost.localdomain>
2021-08-29 18:48:44 +0200

    Make curl less verbose

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # verify that connection is routet through VPN
-curl ipv4.icanhazip.com
-curl ipv6.icanhazip.com
-curl https://raw.githubusercontent.com/macvk/dnsleaktest/master/dnsleaktest.sh -o dnsleaktest.sh
-chmod +x dnsleaktest.sh
-./dnsleaktest.sh
+echo $(curl -s ipv4.icanhazip.com)
+echo $(curl -s ipv6.icanhazip.com)
+#curl https://raw.githubusercontent.com/macvk/dnsleaktest/master/dnsleaktest.sh -o /config/dnsleaktest.sh
+#chmod +x /config/dnsleaktest.sh
+#./config/dnsleaktest.sh
 # run jackett using .net core
 /usr/lib/jackett/jackett --NoRestart --NoUpdates --DataFolder /config/Jackett
