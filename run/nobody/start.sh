2a8e1fc813df375acdd7d47cbb41041ae4dfcdb5
flix <flix@localhost.localdomain>
2021-08-29 18:36:02 +0200

    Added IP checks

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# verify that connection is routet through VPN
+curl ipv4.icanhazip.com
+curl ipv6.icanhazip.com
+curl https://raw.githubusercontent.com/macvk/dnsleaktest/master/dnsleaktest.sh -o dnsleaktest.sh
+chmod +x dnsleaktest.sh
+./dnsleaktest.sh
 # run jackett using .net core
 /usr/lib/jackett/jackett --NoRestart --NoUpdates --DataFolder /config/Jackett
